4b677d64cdcadba5a32b8a37d9473ef42f2f8014
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-07 11:55:07 -0800

    finalized "advanced" sort functionality as part of the question

--- sort.js
+++ sort.js
@@ -3,3 +3,43 @@ var list = [5, 2, 1, 8, 100];
 list.sort((a, b) => a - b);
 
 console.log(list);
+
+var students = [{
+    id: 1,
+    name: "bruce",
+    age: 40
+  },
+  {
+    id: 2,
+    name: "zoidberg",
+    age: 22
+  },
+  {
+    id: 3,
+    name: "alex",
+    age: 22
+  },
+  {
+    id: 4,
+    name: "alex",
+    age: 30
+  }
+];
+
+students.sort(function (a, b) {
+  var persA = a.name.toLowerCase();
+  var persB = b.name.toLowerCase();
+
+  if (persA < persB) {
+    return -1;
+  } else if (persA > persB) {
+    return 1;
+  } else if (persA === persB) {
+    if (a.age > b.age) {
+      return -1;
+    }
+    return 1;
+  }
+});
+
+console.log(students);
